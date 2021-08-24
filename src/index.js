cd3bb636557ed5a334d80145699c2858e381acfd
Дмитрий <76389902+BAKERholy@users.noreply.github.com>
2021-08-24 17:20:21 +0300

    laid out a block with a slider

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import "./scss/style.scss";
 import $ from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
-import "babel";
+
+
+let modalButton = document.querySelector(".modal__button");
+let closeButton = document.querySelector(".close__button");
+let modal = document.querySelector(".main-modal");
+
+modalButton.addEventListener("click",(e) => {
+    e.preventDefault()
+    modal.classList.add("active")
+});
+
+closeButton.addEventListener("click",(e) => {
+    e.preventDefault()
+    modal.classList.remove("active")
+});
+
